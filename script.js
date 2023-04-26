04b2c8b9ecdf0507d4a9546eb6798ad837a61ff1
wjhulzebosch <wjhulzebosch@gmail.com>
2023-04-26 15:43:10 +0200

    Reordered everything

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const levelNumberInput = document.getElementById('level-number');
-const levelNameInput = document.getElementById('level-name');
-const levelDescriptionInput = document.getElementById('level-description');
-const grid = document.getElementById('grid');
-const generateXmlBtn = document.getElementById('generate-xml');
-const xmlOutput = document.getElementById('xml-output');
-const toggleDrawBtn = document.getElementById('toggle-draw');
-const enemyButtons = document.getElementById('enemy-buttons');
-const enemyTypes = ['Horse', 'Ghost', 'Teleporter', 'Bouncer', 'LaserTurret', 'Scanner', 'Speedster', 'ZigZag'];
-const collectableButtons = document.getElementById('collectable-buttons');
-const collectables = ['Key', 'Life', 'Coin', 'Finish'];
-const toggleDeleteBtn = document.getElementById('toggle-delete');
-const tooltip = document.getElementById('tooltip');
-const tooltipOffset = { x: 15, y: 15 };
-
+// Constants
 const gridHeight = 50;
 const gridWidth = 120;
 
@@ -36,15 +22,6 @@ const Tooltip = {
     REMOVE: 'Click the grid to remove walls, enemies, and collectables'
 };
 
-let lastPlacedKeyCell;
-
-let selectedCollectable = null;
-let selectedEnemy = null;
-let isMouseDown = false;
-let canDraw = false;
-let currentState = State.SELECT_ACTION;
-
-
 const collectableSymbols = {
     Key: '🔑',
     Life: '❤️',
@@ -63,31 +40,34 @@ const enemySymbols = {
     ZigZag: '↯'
 };
 
-function createBorderWalls() {
-    for (let i = 0; i < gridHeight; i++) {
-        for (let j = 0; j < gridWidth; j++) {
-            if (i === 0 || i === gridHeight - 1 || j === 0 || j === gridWidth - 1) {
-                const cell = document.querySelector(`.cell[data-top="${i}"][data-left="${j}"]`);
-                cell.classList.add('wall');
-            }
-        }
-    }
-}
+// Variables
+let lastPlacedKeyCell;
+let selectedCollectable = null;
+let selectedEnemy = null;
+let isMouseDown = false;
+let canDraw = false;
+let currentState = State.SELECT_ACTION;
 
-function toggleDelete() {
-    if (currentState === State.DELETE) {
-        currentState = State.SELECT_ACTION;
-        toggleDeleteBtn.textContent = 'Enable Delete';
-    } else {
-        currentState = State.DELETE;
-        toggleDeleteBtn.textContent = 'Disable Delete';
-        canDraw = false;
-        toggleDrawBtn.textContent = 'Enable Drawing';
-        selectedEnemy = null;
-        selectedCollectable = null;
-    }
-}
+// DOM Elements
+const tooltipOffset = { x: 15, y: 15 };
 
+const grid = document.getElementById('grid');
+const levelNumberInput = document.getElementById('level-number');
+const levelNameInput = document.getElementById('level-name');
+const levelDescriptionInput = document.getElementById('level-description');
+const generateXmlBtn = document.getElementById('generate-xml');
+const xmlOutput = document.getElementById('xml-output');
+const toggleDrawBtn = document.getElementById('toggle-draw');
+const enemyButtons = document.getElementById('enemy-buttons');
+const enemyTypes = ['Horse', 'Ghost', 'Teleporter', 'Bouncer', 'LaserTurret', 'Scanner', 'Speedster', 'ZigZag'];
+const collectableButtons = document.getElementById('collectable-buttons');
+const collectables = ['Key', 'Life', 'Coin', 'Finish'];
+const toggleDeleteBtn = document.getElementById('toggle-delete');
+const tooltip = document.getElementById('tooltip');
+const clearEverythingBtn = document.getElementById('clear-everything');
+const drawBorderBtn = document.getElementById('draw-border');
+
+// Functions
 function createGrid() {
     for (let i = 0; i < gridHeight; i++) {
         for (let j = 0; j < gridWidth; j++) {
@@ -99,15 +79,6 @@ function createGrid() {
         }
     }
 }
-
-function toggleDraw() {
-    canDraw = !canDraw;
-    toggleDrawBtn.textContent = canDraw ? 'Disable Drawing' : 'Enable Drawing';
-
-    // Update currentState when toggleDraw is called
-    currentState = canDraw ? State.PLACE_WALL : State.SELECT_ACTION;
-}
-
 function addEnemyButtons() {
     enemyTypes.forEach((enemyType) => {
         const enemyButton = document.createElement('button');
@@ -152,6 +123,28 @@ function addCollectableButtons() {
     });
 }
 
+function toggleDraw() {
+    canDraw = !canDraw;
+    toggleDrawBtn.textContent = canDraw ? 'Disable Drawing' : 'Enable Drawing';
+
+    // Update currentState when toggleDraw is called
+    currentState = canDraw ? State.PLACE_WALL : State.SELECT_ACTION;
+}
+
+function toggleDelete() {
+    if (currentState === State.REMOVE) {
+        currentState = State.SELECT_ACTION;
+        toggleDeleteBtn.textContent = 'Enable Delete';
+    } else {
+        currentState = State.REMOVE;
+        toggleDeleteBtn.textContent = 'Disable Delete';
+        canDraw = false;
+        toggleDrawBtn.textContent = 'Enable Drawing';
+        selectedEnemy = null;
+        selectedCollectable = null;
+    }
+}
+
 function handleCellClick(e) {
     const cell = e.target;
     if (cell.classList.contains('cell')) {
@@ -171,7 +164,7 @@ function handleCellClick(e) {
             cell.style.backgroundColor = 'red';
             lastPlacedKeyCell.dataset.connectedWall = `${cell.dataset.top}-${cell.dataset.left}`;
             currentState = State.SELECT_ACTION;
-        } else if (currentState === State.DELETE) {
+        } else if (currentState === State.REMOVE) {
 
             // Check if the cell contains a key collectable
             if (cell.dataset.collectable === 'Key') {
@@ -204,6 +197,17 @@ function handleCellClick(e) {
 function handleCellMouseOver(e) {
     if (isMouseDown && canDraw) {
         handleCellClick(e);
+    }
+}
+
+function createBorderWalls() {
+    for (let i = 0; i < gridHeight; i++) {
+        for (let j = 0; j < gridWidth; j++) {
+            if (i === 0 || i === gridHeight - 1 || j === 0 || j === gridWidth - 1) {
+                const cell = document.querySelector(`.cell[data-top="${i}"][data-left="${j}"]`);
+                cell.classList.add('wall');
+            }
+        }
     }
 }
 
@@ -325,7 +329,7 @@ function loadXml() {
     }
 }
 
-
+// Event listeners
 grid.addEventListener('mousedown', (e) => {
     isMouseDown = true;
     handleCellClick(e);
@@ -335,8 +339,6 @@ grid.addEventListener('mouseup', () => {
     isMouseDown = false;
 });
 
-const clearEverythingBtn = document.getElementById('clear-everything');
-const drawBorderBtn = document.getElementById('draw-border');
 toggleDeleteBtn.addEventListener('click', toggleDelete);
 
 clearEverythingBtn.addEventListener('click', clearGrid);
@@ -362,9 +364,9 @@ document.addEventListener('mousemove', (e) => {
     } else if (currentState === State.JOIN_KEY_WALL) {
         content = `Click on a wall to join it with the key`;
     } else if (currentState === State.PLACE_WALL) {
-        content = `Click on a cell to place a wall`;
-    } else if (currentState === State.DELETE) {
-        content = `Click on a cell to remove it's contents'`;
+        content = `Draw walls`;
+    } else if (currentState === State.REMOVE) {
+        content = `Delete'`;
     }
 
     tooltip.textContent = content;
